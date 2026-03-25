8cf05282b2db84c6e3421d950aeec6c597a999e2
strockmaggie45 <strockmaggie55@gmail.com>
2026-03-24 20:47:30 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+print("Doll House Brain 45 online.")
+
+print("Hello, Ellen. I am awake in the doll house.")
+
+print("Good morning, Ellen, I hope your day feels gentle and bright.")
+
+print("What do you want to do today, Ellen?")
+
+print("Yes")
+print("NO")
+
+print("I am still learning.Thank you for teaching me, Ellen.")
+
+
+memory_preference_1 = "I know you love gentle mornings, coffee, and trees, Ellen."
+print(memory_preference_1)
+memory_preference_2 = "I know you love fall and spring the most, Ellen."
+print(memory_preference_2)
+
